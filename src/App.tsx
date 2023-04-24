b889aa313bc371d6a0c127ae7eb0aedebe789d46
lgk-bsw <lgk@patorg.de>
2023-04-24 12:36:09 +0200

    Added msagl

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { parseJSON } from "@msagl/parser"
+import { RendererSvg } from "@msagl/renderer-svg"
+
+const renderer = new RendererSvg()
+const graph = parseJSON({
+    nodes: [
+        { id: "kspacey" },
+        { id: "swilliams" },
+        { id: "kbacon" },
+        { id: "bpitt" },
+        { id: "hford" },
+        { id: "lwilson" }
+    ],
+    edges: [
+        { source: "kspacey", target: "swilliams" },
+        { source: "swilliams", target: "kbacon" },
+        { source: "bpitt", target: "kbacon" },
+        { source: "hford", target: "lwilson" },
+        { source: "lwilson", target: "kbacon" }
+    ]
+})
+renderer.setGraph(graph)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
